a7c56cb5193407f24b47d3e7647798ac4906d1ee
Arthur Pochart <62388835+arthurpochart@users.noreply.github.com>
2023-11-27 16:35:30 +0100

    Update grabber.py

--- grabber.py
+++ grabber.py
@@ -6,10 +6,10 @@ claw = MediumMotor(OUTPUT_C)
 myspeed = 50
 
 def open_claw():
-    claw.on_for_degrees(speed=myspeed,degrees=50)
+    claw.on_for_degrees(speed=myspeed,degrees=360)
 
 def close_claw():
-    claw.on_for_degrees(speed=-myspeed,degrees=50)
+    claw.on_for_degrees(speed=-myspeed,degrees=360)
 
 close_claw()
 print("Closing")
