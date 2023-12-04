ecf0da1edd564bd9f7ed83e8414005b441323cd6
Arthur Pochart <62388835+arthurpochart@users.noreply.github.com>
2023-12-04 13:29:01 +0100

    Update c2.py

--- c2.py
+++ c2.py
@@ -1,15 +1,19 @@
 from ev3dev2.motor import MediumMotor, OUTPUT_C, OUTPUT_A, OUTPUT_B
 from time import sleep
 from ev3dev2.motor import LargeMotor, OUTPUT_A, OUTPUT_B, SpeedPercent, MoveTank
+from ev3dev2.sensor.lego import ColorSensor
+from ev3dev2.sensor import INPUT_1
 
 print("System control: ")
 print("Press 1 to start")
 
 drive = MoveTank(OUTPUT_A, OUTPUT_B)
 grabber = MediumMotor(OUTPUT_C)
+colorSensor = ColorSensor()
 
 grabberSpeed = 50
 driveSpeed=50
+onBorder=False
 
 def open_claw():
     grabber.on_for_degrees(speed=grabberSpeed,degrees=360*3)
@@ -17,9 +21,16 @@ def open_claw():
 def close_claw():
     grabber.on_for_degrees(speed=-grabberSpeed,degrees=360*3)
 
+
+def snakeTest():
+    while not onBorder:
+        color = colorSensor.color
+        print(color)
+
+
 escape = False
 while escape==False:
-    direction = input("1.Right 2.Left 3.Open 4.Close")
+    direction = input("1.Right 2.Left 3.Open 4.Close 5.Test")
     
     if direction=='1':
         drive.turn_right(driveSpeed,90)
@@ -29,8 +40,11 @@ while escape==False:
         open_claw()
     elif direction=='4':
         close_claw()
+    elif direction=='5':
+        snakeTest()
     else:
         print("Exiting")
         escape==True
+
 
 
